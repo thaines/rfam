9cc12391c2c54a7383df398dca2a98918bc6ce1a
Tom SF Haines <thaines@gmail.com>
2017-07-01 14:55:29 +0100

    Adding more explicit instructions on how to use node.sh to it's comment.

--- misc/node.sh
+++ misc/node.sh
@@ -1,4 +1,4 @@
-# Single use rfam render node example for a sun grid engine cluster, to be handed to qsub
+# Single use rfam render node example for a sun grid engine cluster, to be handed to qsub. This involves setting the prenode cmd parameter to something like: ssh cluster "qsub -t 1-%i node.sh", noting you will have to escape (backslash) the quotes in the json config file and arrange ssh keys so no password is required.
 #$ -l h_vmem=6G
 #$ -l tmem=6G
 #$ -l h_rt=14400
